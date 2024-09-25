00ea41e4c97a7e190032b4674bd4bc8bd7f07da2
KimGyungSik <0711kyungh@naver.com>
2024-09-25 21:44:28 +0900

    테스트 패키지 구조 변경

--- src/test/java/com/example/demo/post/controller/PostCreateControllerTest.java
+++ src/test/java/com/example/demo/post/controller/PostCreateControllerTest.java
@@ -1,17 +1,13 @@
-package com.example.demo.controller;
+package com.example.demo.post.controller;
 
-import com.example.demo.user.domain.UserCreate;
+import com.example.demo.post.domain.PostCreate;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
@@ -25,38 +21,34 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
 @SqlGroup({
+        @Sql(value = "/sql/post-create-controller-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
         @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 })
-class UserCreateControllerTest {
+class PostCreateControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-    @MockBean
-    private JavaMailSender mailSender;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    
+
     @Test
-    public void 사용자는_회원_가입을_할_수있고_회원가입된_사용자는_PENDING_상태이다() throws Exception {
+    public void 사용자는_게시물을_작성할_수_있다() throws Exception {
         // given
-        UserCreate userCreate = UserCreate.builder()
-                .email("0711kyungh@naver.com")
-                .address("Seoul")
-                .nickname("kyungsik")
+        PostCreate postCreate = PostCreate.builder()
+                .writerId(1)
+                .content("Hi")
                 .build();
-        BDDMockito.doNothing().when(mailSender).send(any(SimpleMailMessage.class));
 
         // when
         // then
-        mockMvc.perform(post("/api/users")
-                        .header("EMAIL","0711kyungh@naver.com")
+        mockMvc.perform(post("/api/posts")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(userCreate))) // jackson serialize해서 컨텐츠로 넘겨줘야함 -> ObjectMapper 필요
+                        .content(objectMapper.writeValueAsString(postCreate))) // jackson serialize해서 컨텐츠로 넘겨줘야함 -> ObjectMapper 필요
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").isNumber())
-                .andExpect(jsonPath("$.email").value("0711kyungh@naver.com"))
-                .andExpect(jsonPath("$.nickname").value("kyungsik"))
-                .andExpect(jsonPath("$.status").value("PENDING"));
+                .andExpect(jsonPath("$.content").value("Hi"))
+                .andExpect(jsonPath("$.writer.id").isNumber())
+                .andExpect(jsonPath("$.writer.email").value("0711kyungh@naver.com"))
+                .andExpect(jsonPath("$.writer.nickname").value("lok22"));
     }
-
 }
--- src/test/java/com/example/demo/user/controller/UserCreateControllerTest.java
+++ src/test/java/com/example/demo/user/controller/UserCreateControllerTest.java
@@ -1,13 +1,17 @@
-package com.example.demo.controller;
+package com.example.demo.user.controller;
 
-import com.example.demo.post.domain.PostCreate;
+import com.example.demo.user.domain.UserCreate;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,34 +25,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
 @SqlGroup({
-        @Sql(value = "/sql/post-create-controller-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
         @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 })
-class PostCreateControllerTest {
+class UserCreateControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private JavaMailSender mailSender;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-
+    
     @Test
-    public void 사용자는_게시물을_작성할_수_있다() throws Exception {
+    public void 사용자는_회원_가입을_할_수있고_회원가입된_사용자는_PENDING_상태이다() throws Exception {
         // given
-        PostCreate postCreate = PostCreate.builder()
-                .writerId(1)
-                .content("Hi")
+        UserCreate userCreate = UserCreate.builder()
+                .email("0711kyungh@naver.com")
+                .address("Seoul")
+                .nickname("kyungsik")
                 .build();
+        BDDMockito.doNothing().when(mailSender).send(any(SimpleMailMessage.class));
 
         // when
         // then
-        mockMvc.perform(post("/api/posts")
+        mockMvc.perform(post("/api/users")
+                        .header("EMAIL","0711kyungh@naver.com")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(postCreate))) // jackson serialize해서 컨텐츠로 넘겨줘야함 -> ObjectMapper 필요
+                        .content(objectMapper.writeValueAsString(userCreate))) // jackson serialize해서 컨텐츠로 넘겨줘야함 -> ObjectMapper 필요
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").isNumber())
-                .andExpect(jsonPath("$.content").value("Hi"))
-                .andExpect(jsonPath("$.writer.id").isNumber())
-                .andExpect(jsonPath("$.writer.email").value("0711kyungh@naver.com"))
-                .andExpect(jsonPath("$.writer.nickname").value("lok22"));
+                .andExpect(jsonPath("$.email").value("0711kyungh@naver.com"))
+                .andExpect(jsonPath("$.nickname").value("kyungsik"))
+                .andExpect(jsonPath("$.status").value("PENDING"));
     }
+
 }
