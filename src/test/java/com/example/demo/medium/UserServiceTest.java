21f6c660bd5d108d0991500c41a6ead083335d8a
KimGyungSik <0711kyungh@naver.com>
2024-09-27 19:30:43 +0900

    UserService를 FakeRepository를 이용하여 테스트

--- src/test/java/com/example/demo/medium/UserServiceTest.java
+++ src/test/java/com/example/demo/medium/UserServiceTest.java
@@ -1,16 +1,13 @@
-package com.example.demo.user.service;
+package com.example.demo.medium;
 
 import com.example.demo.common.domain.exception.CertificationCodeNotMatchedException;
 import com.example.demo.common.domain.exception.ResourceNotFoundException;
-import com.example.demo.mock.FakeMailSender;
-import com.example.demo.mock.FakeUserRepository;
-import com.example.demo.mock.TestClockHolder;
-import com.example.demo.mock.TestUuidHolder;
 import com.example.demo.user.domain.User;
-import com.example.demo.user.domain.UserCreate;
 import com.example.demo.user.domain.UserStatus;
+import com.example.demo.user.domain.UserCreate;
 import com.example.demo.user.domain.UserUpdate;
-import org.junit.jupiter.api.BeforeEach;
+import com.example.demo.user.infrastructure.UserEntity;
+import com.example.demo.user.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,43 +19,20 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 
+@SpringBootTest
+@TestPropertySource("classpath:test-application.properties")
+@SqlGroup({
+        @Sql(value = "/sql/user-service-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
 class UserServiceTest {
-
-    private UserService userService; // 테스트 픽스처(테스트 대상)
-
-    @BeforeEach
-    void init() {
-        FakeMailSender fakeMailSender = new FakeMailSender();
-        FakeUserRepository fakeUserRepository = new FakeUserRepository();
-        this.userService = UserService.builder()
-                .certificationService(new CertificationService(fakeMailSender))
-                .clockHolder(new TestClockHolder(1678530673958L))
-                .uuidHolder(new TestUuidHolder("aaaaaa"))
-                .userRepository(fakeUserRepository)
-                .build();
-        fakeUserRepository.save(User.builder()
-                .id(1L)
-                .email("0711kyungh@naver.com")
-                .nickname("lok22")
-                .address("Seoul")
-                .certificationCode("aaaaaa-aaaaaa-aaaaaaa")
-                .status(UserStatus.ACTIVE)
-                .lastLoginAt(0L)
-                .build());
-        fakeUserRepository.save(User.builder()
-                .id(2L)
-                .email("0712kyungh@naver.com")
-                .nickname("lok23")
-                .address("Seoul")
-                .certificationCode("aaaaaa-aaaaaa-aaaaaab")
-                .status(UserStatus.PENDING)
-                .lastLoginAt(0L)
-                .build());
-    }
+    @Autowired
+    private UserService userService;
+    @MockBean
+    private JavaMailSender mailSender;
 
     @Test
     public void getByEmail은_ACTIVE_상태인_유저를_찾아올_수_있다() throws Exception {
@@ -108,14 +82,14 @@ class UserServiceTest {
                 .address("GEQ")
                 .nickname("saaa")
                 .build();
-
+        BDDMockito.doNothing().when(mailSender).send(any(SimpleMailMessage.class));
         //when
         User result = userService.create(userCreate);
 
         //then
         assertThat(result.getId()).isNotNull();
         assertThat(result.getStatus()).isEqualTo(UserStatus.PENDING);
-        assertThat(result.getCertificationCode()).isEqualTo("aaaaaa");
+//        assertThat(result.getCertificationCode()).isEqualTo("??"); FIXME
     }
     @Test
     public void userUpdateDto_를_이용하여_유저를_수정할_수_있다() throws Exception {
@@ -142,7 +116,8 @@ class UserServiceTest {
 
         //then
         User user = userService.getById(1);
-        assertThat(user.getLastLoginAt()).isEqualTo(1678530673958L);
+        assertThat(user.getLastLoginAt()).isGreaterThan(0L);
+//        assertThat(userEntity.getLastLoginAt()).isEqualTo(????); FIXME
     }
     @Test
     public void PENDING_상태의_사용자는_인증_코드로_ACTIVE_시킬_수_있다() throws Exception {
